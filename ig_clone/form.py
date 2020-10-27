fb572e3fdf23aa0bc67fa6209129094a1cf5d9d0
Ronny <mwithiamoki@gmail.com>
2020-10-27 19:52:41 +0300

    added class imageprofileform

--- ig_clone/form.py
+++ ig_clone/form.py
@@ -5,3 +5,7 @@ class ImageUploadForm(forms.ModelForm):
     class Meta:
         model = Image
         exclude = ['profile','likes','comments','user']
+class ImageProfileForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        exclude = ['user',]        
