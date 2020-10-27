5576b1ab44e1c84d349193005c54205c8a256c54
Ronny <mwithiamoki@gmail.com>
2020-10-27 19:40:55 +0300

    defined image_uploads

--- ig_clone/views.py
+++ ig_clone/views.py
@@ -14,3 +14,17 @@ def home(request):
    
     
     return render(request, 'instagram/index.html',{"images":images})
+
+def image_upload(request):
+    current_user = request.user
+    if request.method == 'POST':
+        form = ImageUploadForm(request.POST,request.FILES)
+        if form.is_valid():
+            image = form.save(commit=False)
+            image.user = current_user
+            image.save()
+        return redirect('home')
+
+    else:
+        form = ImageUploadForm()
+        return render(request,'socioapp/upload.html', {"form":form})
