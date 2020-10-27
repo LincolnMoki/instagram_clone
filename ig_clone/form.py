8f6ced0a9a77c27df756d08b0222c9a9df936174
Ronny <mwithiamoki@gmail.com>
2020-10-27 19:51:48 +0300

    created form and added class imageuploadform

---
+++ ig_clone/form.py
@@ -0,0 +1,7 @@
+from django import forms
+from . models import Image,Profile,Comments
+
+class ImageUploadForm(forms.ModelForm):
+    class Meta:
+        model = Image
+        exclude = ['profile','likes','comments','user']
